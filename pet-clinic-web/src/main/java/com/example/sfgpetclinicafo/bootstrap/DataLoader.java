8c805ec95762fb0eccccc9c02ebf5a78bfc32404
fofanaabou <aboufofana76@gmail.com>
2022-06-09 00:23:53 +0200

    Adding bootstrap for data. closes #14

---
+++ pet-clinic-web/src/main/java/com/example/sfgpetclinicafo/bootstrap/DataLoader.java
@@ -0,0 +1,63 @@
+package com.example.sfgpetclinicafo.bootstrap;
+
+import com.example.sfgpetclinic.model.Owner;
+import com.example.sfgpetclinic.model.Vet;
+import com.example.sfgpetclinic.model.map.OwnerServiceMap;
+import com.example.sfgpetclinic.model.map.VetServiceMap;
+import com.example.sfgpetclinic.services.OwnerService;
+import com.example.sfgpetclinic.services.VetService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private  final OwnerService ownerService;
+    private  final VetService vetService;
+
+    public DataLoader() {
+        ownerService = new OwnerServiceMap();
+        vetService = new VetServiceMap();
+    }
+
+    @Override
+    public void run(String... args) {
+        Owner owner1 = new Owner();
+        owner1.setId(1L);
+        owner1.setFirstName("Michael");
+        owner1.setLastName("Weston");
+
+        ownerService.save(owner1);
+
+        Owner owner2 = new Owner();
+        owner2.setId(2L);
+        owner2.setFirstName("Fiona");
+        owner2.setLastName("Glenanne");
+
+        ownerService.save(owner2);
+
+        System.out.println("Loading owners......");
+
+        Vet vet1 = new Vet();
+        vet1.setId(1L);
+        vet1.setFirstName("Jessie");
+        vet1.setLastName("Porter");
+
+        vetService.save(vet1);
+
+        Vet vet2 = new Vet();
+        vet2.setId(2L);
+        vet2.setFirstName("Sam");
+        vet2.setLastName("Axe");
+
+        vetService.save(vet2);
+
+        System.out.println("Loading veterinarians.....");
+
+        Set<Owner> owners = ownerService.findAll();
+        owners.forEach(owner -> System.out.println("name:" + owner.getFirstName()));
+
+    }
+}
