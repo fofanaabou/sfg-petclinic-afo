001fcac282dd80f3fa6431ee35a796e0aec201c9
fofanaabou <aboufofana76@gmail.com>
2022-06-14 23:52:38 +0200

    added specialties, added to vet. closes #27

---
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/Specialty.java
@@ -0,0 +1,14 @@
+package com.example.sfgpetclinic.model;
+
+public class Specialty extends BaseEntity{
+
+    private String getDescription;
+
+    public String getGetDescription() {
+        return getDescription;
+    }
+
+    public void setGetDescription(String getDescription) {
+        this.getDescription = getDescription;
+    }
+}
