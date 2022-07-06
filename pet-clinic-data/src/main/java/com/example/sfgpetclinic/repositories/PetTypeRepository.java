72d66265ec20102c0b0aba4275bf21a346e048d0
fofanaabou <aboufofana76@gmail.com>
2022-07-06 14:59:40 +0200

    Implemented repos. closes #39

---
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/repositories/PetTypeRepository.java
@@ -0,0 +1,6 @@
+package com.example.sfgpetclinic.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PetTypeRepository extends CrudRepository<PetTypeRepository, Long> {
+}
