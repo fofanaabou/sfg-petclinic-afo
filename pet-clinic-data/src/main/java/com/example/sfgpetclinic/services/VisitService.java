005c706067266250ce1f51bacc69d0096c84d1c3
dev_afo <aboufofana76@gmail.com>
2022-07-08 12:35:18 +0200

    Implemented Map iml for service. closes #46

---
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,8 @@
+package com.example.sfgpetclinic.services;
+
+import com.example.sfgpetclinic.model.Visit;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface VisitService extends CrudService<Visit, Long>{
+}
